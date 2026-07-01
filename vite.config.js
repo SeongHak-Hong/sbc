58cf09e39908b1fb48b0105599b5ba603d2ef090
Seonghak-Hong <sung3045@naver.com>
2026-07-01 15:15:54 +0900

    Fix Vite base path for Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: command === 'serve' ? '/' : '/sbc/',
+  base: '/',
 }))
